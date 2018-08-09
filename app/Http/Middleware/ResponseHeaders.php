a2059ee189e003f7f36bb5ed4748ee817b0a0924
FreeScout <freescout.helpdesk@gmail.com>
2018-08-09 02:14:04 -0700

    Disable response cache

---
+++ app/Http/Middleware/ResponseHeaders.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ResponseHeaders {
+
+	public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        // Disable caching
+        $response->header("Pragma", "no-cache");
+        $response->header("Cache-Control", "no-cache, max-age=0, must-revalidate, no-store");
+
+        return $response;
+    }
+}
