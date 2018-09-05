7914216907650f81332b4d19286089635a1eaebd
FreeScout <freescout.helpdesk@gmail.com>
2018-09-05 03:57:46 -0700

    Fix remember me checkbox

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,11 +43,9 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                                </label>
                             </div>
                         </div>
 
