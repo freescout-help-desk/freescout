e2c2be7d71fd6acd797ef5cea90cf40501ea311d
FreeScout <freescout.helpdesk@gmail.com>
2018-09-18 00:37:34 -0700

    Fix error in migration on MariaDB: Specified key was too long

--- database/migrations/2018_07_09_053559_create_customers_table.php
+++ database/migrations/2018_07_09_053559_create_customers_table.php
@@ -39,7 +39,7 @@ class CreateCustomersTable extends Migration
 
             // Indexes
             // For ajax search
-            $table->index(['first_name', 'last_name']);
+            $table->index([DB::raw('first_name(191)'), DB::raw('last_name(191)')]);
         });
     }
 
