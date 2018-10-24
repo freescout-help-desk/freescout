8efcaedb42bd75ae9bded2d9366bb6b141fcf197
FreeScout <freescout.helpdesk@gmail.com>
2018-10-24 02:54:45 -0700

    Make aliases non-required in mailbox settings

--- resources/views/mailboxes/update.blade.php
+++ resources/views/mailboxes/update.blade.php
@@ -46,7 +46,7 @@
 
                         <div class="col-sm-6">
                             <div class="flexy">
-                                <input id="aliases" type="text" class="form-control input-sized" name="aliases" value="{{ old('aliases', $mailbox->aliases) }}" maxlength="255" required autofocus>
+                                <input id="aliases" type="text" class="form-control input-sized" name="aliases" value="{{ old('aliases', $mailbox->aliases) }}" maxlength="255">
 
                                 <i class="glyphicon glyphicon-info-sign icon-info" data-toggle="popover" data-trigger="hover" data-html="true" data-placement="left"  data-content="{{ __('Aliases are other email addresses that also forward to your mailbox address. Separate each email with a comma.') }}"></i>
                             </div>
