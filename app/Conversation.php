80ae8f117f8b76d2d5edeee0d642bae238f22ad0
Alessandro Corbelli <alessandro.corbelli@guest.it>
2018-11-09 10:28:08 +0100

    Allow to set the next conversation number

--- app/Conversation.php
+++ app/Conversation.php
@@ -162,7 +162,14 @@ class Conversation extends Model
         parent::boot();
 
         self::creating(function (Conversation $model) {
-            $model->number = Conversation::max('number') + 1;
+            $next_ticket = (int)Option::get('next_ticket');
+            Option::remove('next_ticket');
+
+            // If next_ticket is set and available, set that.
+            if ( !$next_ticket || Conversation::where('number', $next_ticket)->exists() )
+                $next_ticket = Conversation::max('number') + 1;
+
+            $model->number = $next_ticket;
         });
     }
 
