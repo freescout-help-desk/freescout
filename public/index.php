074f98f37564b49dfa4ae625786ed9784e716c1b
FreeScout <freescout.helpdesk@gmail.com>
2018-11-11 21:50:43 -0800

    Check php version in index.php and artisan

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+// Check PHP version
+if (!version_compare(phpversion(), '7.0.0', '>=')) {
+	echo 'PHP 7.x is required to run FreeScout.';
+	exit();
+}
+
 /**
  * Laravel - A PHP Framework For Web Artisans.
  *
