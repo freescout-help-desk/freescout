cfc59939f7e9f4564f53e0923ffdbfa09055f5b0
FreeScout <freescout.helpdesk@gmail.com>
2018-11-13 05:53:28 -0800

    Email Template option hidden until somebody needs it

--- resources/views/mailboxes/update.blade.php
+++ resources/views/mailboxes/update.blade.php
@@ -96,7 +96,8 @@
                         </div>
                     </div>
 
-                    <div class="form-group{{ $errors->has('template') ? ' has-error' : '' }}">
+                    {{-- Email Template option hidden until somebody needs it --}}
+                    <div class="form-group{{ $errors->has('template') ? ' has-error' : '' }}" style="display:none">
                         <label for="template" class="col-sm-2 control-label">{{ __('Email Template') }} (todo)</label>
 
                         <div class="col-sm-6">
