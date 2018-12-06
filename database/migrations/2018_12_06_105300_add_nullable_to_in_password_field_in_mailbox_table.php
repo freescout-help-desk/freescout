7c8a25cd4c0a7feb5a20c90e7c458e5053f90adf
Alessandro Corbelli <alessandro.corbelli@guest.it>
2018-12-06 11:03:03 +0100

    Add nullable to in_password field in mailboxes table

---
+++ database/migrations/2018_12_06_105300_add_nullable_to_in_password_field_in_mailbox_table.php
@@ -0,0 +1,32 @@
+in<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNullableToInPasswordFieldInMailboxTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mailboxes', function($table) {
+            $table->string('in_password', 512)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mailboxes', function($table) {
+            $table->string('in_password', 512)->nullable(false)->change();
+        });
+    }
+}
