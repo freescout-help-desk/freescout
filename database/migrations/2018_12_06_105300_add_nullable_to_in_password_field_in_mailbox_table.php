d3ce3e8722c7a9abff05a7faf150169948e58720
Alessandro Corbelli <alessandro.corbelli@guest.it>
2018-12-06 12:47:51 +0100

    Fix styleci

--- database/migrations/2018_12_06_105300_add_nullable_to_in_password_field_in_mailbox_table.php
+++ database/migrations/2018_12_06_105300_add_nullable_to_in_password_field_in_mailbox_table.php
@@ -1,7 +1,6 @@
 in<?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class AddNullableToInPasswordFieldInMailboxTable extends Migration
