326d37cf1c5d62ec64cd301a392d6e359fc603af
Junker <junker@front.ru>
2018-12-20 13:29:51 +0100

    show table tags(table,tr,td) in conversation email body

--- config/purifier.php
+++ config/purifier.php
@@ -24,7 +24,7 @@ return [
     'settings'      => [
         'default' => [
             'HTML.Doctype'             => 'HTML 4.01 Transitional',
-            'HTML.Allowed'             => 'div,b,strong,i,em,u,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
+            'HTML.Allowed'             => 'div,b,strong,i,em,u,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src],table,tr,td',
             //'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
             'CSS.AllowedProperties'    => 'font-weight,font-style,text-decoration,color,background-color,text-align',
             'AutoFormat.AutoParagraph' => true,
