0be7557204bac1eba33e9dd3b89c01669fd5b8c5
Alessandro Corbelli <alessandro.corbelli@guest.it>
2018-12-21 18:14:56 +0100

    Increased log

--- app/Console/Commands/FetchEmails.php
+++ app/Console/Commands/FetchEmails.php
@@ -244,6 +244,7 @@ class FetchEmails extends Command
                         // Detect bounce by attachment.
                         // Check all attachments.
                         foreach ($attachments as $attachment) {
+                            $this->line('['.date('Y-m-d H:i:s').'] Found attachment. Type='.$attachment->getType().', content-type='.$attachment->content_type);
                             if (!empty(Attachment::$types[$attachment->getType()]) && Attachment::$types[$attachment->getType()] == Attachment::TYPE_MESSAGE
                             ) {
                                 if (
@@ -277,6 +278,8 @@ class FetchEmails extends Command
                             $original_from = $this->formatEmailList($message->getFrom());
                             $original_from = $original_from[0];
                             $is_bounce = preg_match('/^mailer\-daemon@/i', $original_from);
+
+                            $this->line('['.date('Y-m-d H:i:s').'] original_from: '.$original_from);
                         }
                     }
 
