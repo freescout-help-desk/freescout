fac3713af4ae113d110e21a4ff5a90bb13adea6f
FreeScout <freescout.helpdesk@gmail.com>
2019-06-21 22:42:00 -0700

    Fix Class Helper not found in translation-manager.php

--- config/translation-manager.php
+++ config/translation-manager.php
@@ -11,7 +11,7 @@ return [
     |
     */
     'route'          => [
-        'prefix'     => \Helper::getSubdirectory(true).'translations',
+        'prefix'     => trim(parse_url(env('APP_URL', ''), PHP_URL_PATH), '/').'/translations',
         'middleware' => [
             'web',
             'auth',
