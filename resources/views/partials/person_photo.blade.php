b0063d2c2e4be33712056ae9a3fd62501840961e
FreeScout <freescout.helpdesk@gmail.com>
2019-08-02 06:29:20 -0700

    Fix utf8 user/customer initials

--- resources/views/partials/person_photo.blade.php
+++ resources/views/partials/person_photo.blade.php
@@ -2,4 +2,4 @@
 	@if (!empty($person->role))
 		@if ($person->photo_url)
 	        <img class="person-photo" src="{{ $person->getPhotoUrl() }}" />@else
-	        <i class="person-photo person-photo-auto" data-initial="{{ strtoupper($person->first_name[0]) }}{{ strtoupper($person->last_name[0]) }}"></i>@endif{{ '' }}@else<img class="person-photo" src="{{ $person->getPhotoUrl() }}" alt="">@endif{{ '' }}@endif
+	        <i class="person-photo person-photo-auto" data-initial="{{ strtoupper(mb_substr($person->first_name, 0, 1)) }}{{ strtoupper(mb_substr($person->last_name, 0, 1)) }}"></i>@endif{{ '' }}@else<img class="person-photo" src="{{ $person->getPhotoUrl() }}" alt="">@endif{{ '' }}@endif
