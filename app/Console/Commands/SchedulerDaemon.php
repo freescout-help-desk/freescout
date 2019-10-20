532fd87ae2b581a1e9ce9b5417057b2f8d90b330
Julien DAUPHANT <jdauphant@users.noreply.github.com>
2019-10-20 21:00:43 +0200

    Create SchedulerDaemon.php

---
+++ app/Console/Commands/SchedulerDaemon.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class SchedulerDaemon extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'schedule:daemon {--sleep=60}';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Call the scheduler every minute.';
+
+  /**
+   * Execute the console command.
+   *
+   * @return mixed
+   */
+  public function handle()
+  {
+      while (true) {
+          $this->line('<info>[' . Carbon::now()->format('Y-m-d H:i:s') . ']</info> Calling scheduler');
+
+          $this->call('schedule:run');
+
+          sleep($this->option('sleep'));
+      }
+  }
+}
