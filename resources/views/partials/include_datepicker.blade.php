e38ccdee96a793b4d9ad9a44693ebf54106a5cd4
FreeScout <freescout.helpdesk@gmail.com>
2019-12-02 06:25:01 -0800

    Sortable panel

---
+++ resources/views/partials/include_datepicker.blade.php
@@ -0,0 +1,9 @@
+@section('stylesheets')
+	@parent
+    <link href="{{ asset('js/flatpickr/flatpickr.min.css') }}" rel="stylesheet">
+@endsection
+
+@section('javascripts')
+    @parent
+    {!! Minify::javascript(['/js/flatpickr/flatpickr.min.js', '/js/flatpickr/l10n/'.strtolower(Config::get('app.locale')).'.js']) !!}
+@endsection
