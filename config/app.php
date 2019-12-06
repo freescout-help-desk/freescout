a15e87f23c06e281fc5e62b9bae1eaf10ae9cf82
FreeScout <freescout.helpdesk@gmail.com>
2019-12-06 01:57:42 -0800

    Add ru language

--- config/app.php
+++ config/app.php
@@ -92,7 +92,7 @@ return [
     */
 
     'locale'          => env('APP_LOCALE', 'en'),
-    'locales'         => ['en', 'fr', 'it', 'pt-PT', 'pt-BR'],
+    'locales'         => ['en', 'fr', 'it', 'pt-PT', 'pt-BR', 'ru'],
     'default_locale'  => 'en',
 
     /*
