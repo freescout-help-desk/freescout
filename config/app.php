1cd8881088d9d9f220204d8a24db76c785c8ebd3
FreeScout <freescout.helpdesk@gmail.com>
2019-12-08 09:00:46 -0800

    v1.3.13

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '1.3.12',
+    'version' => '1.3.13',
 
     /*
     |--------------------------------------------------------------------------
