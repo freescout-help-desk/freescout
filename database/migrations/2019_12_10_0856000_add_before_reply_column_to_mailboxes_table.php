53138e405eaa2f497affcca2813908c7bb0a7cbb
FreeScout <freescout.helpdesk@gmail.com>
2019-12-18 23:02:26 -0800

    Fix Row size too large error - closes #393

--- database/migrations/2019_12_10_0856000_add_before_reply_column_to_mailboxes_table.php
+++ database/migrations/2019_12_10_0856000_add_before_reply_column_to_mailboxes_table.php
@@ -13,6 +13,16 @@ class AddBeforeReplyColumnToMailboxesTable extends Migration
      */
     public function up()
     {
+        // To avoid 'Row size too large' error.
+        // https://github.com/freescout-helpdesk/freescout/issues/393
+        // https://dev.mysql.com/doc/refman/8.0/en/column-count-limit.html
+        if (version_compare(config('app.version'), '1.3.17', '<')) {
+            Schema::table('mailboxes', function (Blueprint $table) {
+                $table->text('out_password')->nullable()->change();
+                $table->text('in_password')->nullable()->change();
+            });
+        }
+
         Schema::table('mailboxes', function (Blueprint $table) {
             $table->text('before_reply')->nullable();
         });
@@ -28,5 +38,12 @@ class AddBeforeReplyColumnToMailboxesTable extends Migration
         Schema::table('mailboxes', function (Blueprint $table) {
             $table->dropColumn('before_reply');
         });
+
+        if (version_compare(config('app.version'), '1.3.17', '<')) {
+            Schema::table('mailboxes', function (Blueprint $table) {
+                $table->string('out_password', 255)->nullable()->change();
+                $table->string('in_password', 512)->nullable()->change();
+            });
+        }
     }
 }
