9984c4453fb76dac7745a74d4f98a7255d2f73b7
Timo Stramann <21283057+TimoStramann@users.noreply.github.com>
2020-01-18 11:55:15 +0100

    Added German translation
    Most strings translated into German

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  'password' => 'Das Passwort muss mindestens sechs Zeichen lang sein und die Bestätigungseingabe muss übereinstimmen.',
+  'reset'    => 'Das Passwort wurde zurückgesetzt!',
+  'sent'     => 'Wir haben Ihnen den Link zum Zurücksetzen des Passworts gesendet!',
+  'token'    => 'Der Token zum Zurücksetzen des Passworts ist ungültig.',
+  'user'     => 'Wir können keinen Benutzer mit dieser Email-Adresse finden.',
+];
