39f6c8959e12e7be9d958ed7651be72b1376d20d
FreeScout <freescout.helpdesk@gmail.com>
2020-02-15 02:27:08 -0800

    Swedish language

--- config/app.php
+++ config/app.php
@@ -92,7 +92,7 @@ return [
     */
 
     'locale'          => env('APP_LOCALE', 'en'),
-    'locales'         => ['en', 'nl', 'fr', 'de', 'it', 'pt-PT', 'pt-BR', 'ru', 'es'],
+    'locales'         => ['en', 'nl', 'fr', 'de', 'it', 'pt-PT', 'pt-BR', 'ru', 'es', 'sv'],
     'default_locale'  => 'en',
 
     /*
