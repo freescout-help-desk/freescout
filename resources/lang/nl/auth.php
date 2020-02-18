383cbe953fa8c981c9b7135253a0e7d391e55d15
FreeScout <freescout.helpdesk@gmail.com>
2020-02-17 19:01:35 -0800

    Update dutch translation

---
+++ resources/lang/nl/auth.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+  'failed'   => 'Deze login gegevens komen niet overeen met onze gegevens.',
+  'throttle' => 'Teveel login pogingen. Probeer het aub nog een keer over :seconds seconden.',
+];
---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  'password' => 'Wachtwoorden moeten minsten zes karakters hebben en aan de voorwaarden voldoen.',
+  'reset'    => 'Uw wachtwoord is gereset!',
+  'sent'     => 'We hebben u een wachtwoord reset link gemaild!',
+  'token'    => 'Dit wachtoord reset token is ongeldig.',
+  'user'     => 'We kunnen geen gebruiker met dat e-mail adres vinden.',
+];
