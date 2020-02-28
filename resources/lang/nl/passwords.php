00b4fc53bd69a2b3582e3318ab905b2e21be2d86
Daan Seegers <d.seegers.mail@gmail.com>
2020-02-28 17:10:49 +0100

    Update passwords.php

--- resources/lang/nl/passwords.php
+++ resources/lang/nl/passwords.php
@@ -3,7 +3,7 @@
 return [
   'password' => 'Wachtwoorden moeten minsten zes karakters bevatten en aan de voorwaarden voldoen.',
   'reset'    => 'Uw wachtwoord is gereset!',
-  'sent'     => 'We hebben uw een wachtwoord reset link gemaild!',
+  'sent'     => 'We hebben u een wachtwoord reset link gemaild!',
   'token'    => 'Dit wachtoord reset token is ongeldig.',
   'user'     => 'We kunnen geen gebruiker met dit e-mailadres vinden.',
 ];
