cb139b11ca54e7594dcf0a173d90506f82e9495e
FreeScout <freescout.helpdesk@gmail.com>
2020-03-05 01:44:27 -0800

    Show only native languages in the language dropdown

--- resources/views/partials/locale_options.blade.php
+++ resources/views/partials/locale_options.blade.php
@@ -15,5 +15,5 @@
 	@php
 		$data = \Helper::getLocaleData($locale);
 	@endphp
-	<option value="{{ $locale }}" @if ($selected == $locale)selected="selected"@endif>{{ $data['name_en'] }}@if ($data['name'] != $data['name_en']) ({{ $data['name'] }})@endif</option>
+	<option value="{{ $locale }}" @if ($selected == $locale)selected="selected"@endif>{{ $data['name'] }}</option>
 @endforeach
