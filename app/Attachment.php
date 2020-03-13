d3158cc8a1bcc7f10b1f82ed3626ebb3a7736cb5
FreeScout <freescout.helpdesk@gmail.com>
2020-03-12 22:08:04 -0700

    Fix The filename fallback must only contain ASCII characters - closes #533

--- app/Attachment.php
+++ app/Attachment.php
@@ -208,6 +208,8 @@ class Attachment extends Model
      */
     public function download()
     {
+        // #533
+        //return $this->getDisk()->download($this->getStorageFilePath(), \Str::ascii($this->file_name));
         return $this->getDisk()->download($this->getStorageFilePath(), $this->file_name);
     }
 
