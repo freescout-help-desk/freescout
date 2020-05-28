7451223e09a2bbeb2fea01db34950e856772ba6b
FreeScout <freescout.helpdesk@gmail.com>
2020-05-28 02:21:14 -0700

    Sort threads in emails to customers by thread ID instead of created_at - closes #532

--- resources/views/emails/customer/reply_fancy_text.blade.php
+++ resources/views/emails/customer/reply_fancy_text.blade.php
@@ -3,7 +3,7 @@
 -----------------------------------------------------------
 @if (!$loop->first)## {{--@if ($loop->last){{ __(':person sent a message', ['person' => $thread->getFromName($mailbox)]) }}@else {{ __(':person replied', ['person' => $thread->getFromName($mailbox)]) }}@endif--}}{{ $thread->getFromName($mailbox) }}, {{ __('on :date', ['date' => App\Customer::dateFormat($thread->created_at, 'M j @ H:i')]) }} ({{ \Config::get('app.timezone') }}):@endif 
 {{-- Html2Text\Html2Text::convert($thread->body) - this was causing "AttValue: " expected in Entity" error sometimes --}}{{ (new Html2Text\Html2Text($thread->body))->getText() }}
-@if ($thread->source_via == App\Thread::PERSON_USER)
+@if ($thread->source_via == App\Thread::PERSON_USER && \Eventy::filter('reply_email.include_signature', true, $thread))
 
 {{-- Html2Text\Html2Text::convert($conversation->mailbox->signature) --}}{{ (new Html2Text\Html2Text($conversation->getSignatureProcessed(['thread' => $thread])))->getText() }}
 @endif
