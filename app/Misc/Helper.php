b6fac871e066a2f06b579704f859aeafcb319f02
FreeScout <freescout.helpdesk@gmail.com>
2020-07-26 00:38:44 -0700

    Main menu hooks

--- app/Misc/Helper.php
+++ app/Misc/Helper.php
@@ -1338,4 +1338,19 @@ class Helper
     {
         return \DB::connection()->getPDO()->getAttribute(\PDO::ATTR_DRIVER_NAME) == 'mysql';
     }
+
+    public static function humanFileSize($size, $unit="")
+    {
+        if ((!$unit && $size >= 1<<30) || $unit == "GB") {
+            return number_format($size/(1<<30),2)."GB";
+        }
+        if ((!$unit && $size >= 1<<20) || $unit == "MB") {
+            return number_format($size/(1<<20),2)."MB";
+        }
+        //if ((!$unit && $size >= 1<<10) || $unit == "KB") {
+        return number_format($size/(1<<10),2)."KB";
+        // }
+        // return number_format($size)." bytes";
+    }
+
 }
