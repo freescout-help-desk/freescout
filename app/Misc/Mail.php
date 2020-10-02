fde1e9610466cc0a508f0192acad8824718bb17a
FreeScout <freescout.helpdesk@gmail.com>
2020-10-02 01:18:13 -0700

    Do not cut out regular Gmail quotes - closes #778

--- app/Misc/Mail.php
+++ app/Misc/Mail.php
@@ -66,7 +66,7 @@ class Mail
         'regex:/<div style="border:none;border\-top:solid \#[A-Z0-9]{6} 1\.0pt;padding:3\.0pt 0in 0in 0in">[^<]*<p class="MsoNormal"><b>/', // MS Outlook
 
         // General separators.
-        '<blockquote', // General sepator
+        'regex:/<blockquote((?!quote)[^>])*>/', // General sepator. Should skip Gmail's <blockquote class="gmail_quote">.
         '<!-- originalMessage -->',
         '‐‐‐‐‐‐‐ Original Message ‐‐‐‐‐‐‐',
         '--------------- Original Message ---------------',
