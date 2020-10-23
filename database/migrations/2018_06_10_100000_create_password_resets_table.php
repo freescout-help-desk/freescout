0f4f2674f78daabc9216a6ab31a6dee2f5b293e5
FreeScout <freescout.helpdesk@gmail.com>
2020-10-23 11:39:42 -0700

    Fix sql_require_primary_key issue - closes #818

--- database/migrations/2018_06_10_100000_create_password_resets_table.php
+++ database/migrations/2018_06_10_100000_create_password_resets_table.php
@@ -14,6 +14,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('email', 191)->index();
             $table->string('token', 255);
             $table->timestamp('created_at')->nullable();
