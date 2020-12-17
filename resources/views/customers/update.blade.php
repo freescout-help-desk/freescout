ef8d6ddf07b207915b9e16198f379305c143eff7
FreeScout <freescout.helpdesk@gmail.com>
2020-12-16 23:10:30 -0800

    customer.profile_preview_prepend hook

--- resources/views/customers/update.blade.php
+++ resources/views/customers/update.blade.php
@@ -3,8 +3,11 @@
 @section('title_full', $customer->getFullName(true).' - '.__('Customer Profile'))
 @section('body_class', 'sidebar-no-height')
 
+@section('body_attrs')@parent data-customer_id="{{ $customer->id }}"@endsection
+
 @section('sidebar')
     <div class="profile-preview">
+    	@action('customer.profile_preview_prepend', $customer)
         @include('customers/profile_snippet')
     </div>
 @endsection
