d4127889c33e6c004dbc194e6e03a47651d03f02
FreeScout <freescout.helpdesk@gmail.com>
2021-05-17 06:01:43 -0700

    Filter to disable mailbox sidebar buttons

--- resources/views/mailboxes/sidebar_menu_view.blade.php
+++ resources/views/mailboxes/sidebar_menu_view.blade.php
@@ -9,15 +9,17 @@
 @php
     $show_settings_btn = Auth::user()->can('viewMailboxMenu', Auth::user());
 @endphp
-<div class="sidebar-buttons btn-group btn-group-justified @if ($show_settings_btn) has-settings @endif">
-    @if ($show_settings_btn)
-        <div class="btn-group dropdown" data-toggle="tooltip" title="{{ __("Mailbox Settings") }}">
-            <a class="btn btn-trans dropdown-toggle" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-cog"></i> <b class="caret"></b></a>
-            <ul class="dropdown-menu" role="menu">
-                @include("mailboxes/settings_menu", ['is_dropdown' => true])
-            </ul>
-        </div>
-    @endif
-    <a class="btn btn-trans" href="{{ route('conversations.create', ['mailbox_id' => $mailbox->id]) }}" aria-label="{{ __("New Conversation") }}" data-toggle="tooltip" title="{{ __("New Conversation") }}"><i class="glyphicon glyphicon-envelope"></i></a>
-</div>
+@if (\Eventy::filter('mailbox.show_sidebar_buttons', true, $mailbox))
+    <div class="sidebar-buttons btn-group btn-group-justified @if ($show_settings_btn) has-settings @endif">
+        @if ($show_settings_btn)
+            <div class="btn-group dropdown" data-toggle="tooltip" title="{{ __("Mailbox Settings") }}">
+                <a class="btn btn-trans dropdown-toggle" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-cog"></i> <b class="caret"></b></a>
+                <ul class="dropdown-menu" role="menu">
+                    @include("mailboxes/settings_menu", ['is_dropdown' => true])
+                </ul>
+            </div>
+        @endif
+        <a class="btn btn-trans" href="{{ route('conversations.create', ['mailbox_id' => $mailbox->id]) }}" aria-label="{{ __("New Conversation") }}" data-toggle="tooltip" title="{{ __("New Conversation") }}"><i class="glyphicon glyphicon-envelope"></i></a>
+    </div>
+@endif
 @action('mailbox.after_sidebar_buttons')
