6e900d111c2f884a6b92644b7435ae5a560bcfbd
Alec Kinnear <github@foliovision.com>
2021-06-04 13:55:36 +0200

    Fixing missing Email Customer link in sidebar
    Found proper function

--- resources/views/customers/profile_snippet.blade.php
+++ resources/views/customers/profile_snippet.blade.php
@@ -13,13 +13,13 @@
 			@if (!empty($main_email))
 		    	@foreach ($customer->emails as $email)
 		    		@if ($email->email == $main_email)
-		            	<li class="customer-email"><a href="/mailbox/{{ $mailbox->id }}/new-ticket?to={{ urlencode($email->email) }}" title="{{ __('Email customer') }}" class="contact-main">{{ $email->email }}</a></li>
+		            	<li class="customer-email"><a href="{{ $mailbox->url() }}/new-ticket?to={{ urlencode($email->email) }}" title="{{ __('Email customer') }}" class="contact-main">{{ $email->email }}</a></li>
 		           	@endif
 		        @endforeach
 		    @endif
 		    @foreach ($customer->emails as $email)
 		    	@if (empty($main_email) || $email->email != $main_email)
-	            	<li class="customer-email"><a href="/mailbox/{{ $mailbox->id }}/new-ticket?to={{ urlencode($email->email) }}" title="{{ __('Email customer') }}" class="@if (empty($main_email) && $loop->index == 0) contact-main @endif">{{ $email->email }}</a></li>
+	            	<li class="customer-email"><a href="{{ $mailbox->url() }}/new-ticket?to={{ urlencode($email->email) }}" title="{{ __('Email customer') }}" class="@if (empty($main_email) && $loop->index == 0) contact-main @endif">{{ $email->email }}</a></li>
 	            @endif
 	        @endforeach
 			@foreach ($customer->getPhones() as $phone)
