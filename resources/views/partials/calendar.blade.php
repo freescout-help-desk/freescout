3dc5f1c8438381653c3240c776cde907713f7f9a
FreeScout <freescout.helpdesk@gmail.com>
2021-06-16 10:04:36 -0700

    Replace calendar partial with include_datepicker

--- resources/views/partials/calendar.blade.php
+++
@@ -1,8 +0,0 @@
-@section('stylesheets')
-    <link href="{{ asset('js/flatpickr/flatpickr.min.css') }}" rel="stylesheet">
-@endsection
-
-@section('javascripts')
-    @parent
-    {!! Minify::javascript(['/js/flatpickr/flatpickr.min.js', '/js/flatpickr/l10n/'.strtolower(Config::get('app.locale')).'.js']) !!}
-@endsection
