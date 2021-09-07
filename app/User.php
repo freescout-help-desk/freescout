db6fb0513cb13bec1349d9aecbde777d71e17f02
Charrier <lucas.charrier@beta.gouv.fr>
2021-09-07 16:27:15 +0200

    Update User.php

--- app/User.php
+++ app/User.php
@@ -805,7 +805,8 @@ class User extends Authenticatable
         ob_start();
         imagejpeg($resized_image, $dest_path, self::PHOTO_QUALITY);
         $image_contents = ob_get_clean();
-        Storage::put(self::PHOTO_DIRECTORY.DIRECTORY_SEPARATOR.$file_name, $image_contents);
+        // Storage::put(self::PHOTO_DIRECTORY.DIRECTORY_SEPARATOR.$file_name, $image_contents);
+        Storage::put($file_name, $uploaded_file, 'public')
         // imagejpeg($resized_image, $dest_path, self::PHOTO_QUALITY);
         // $photo_url = $request->file('photo_url')->storeAs(
         //     User::PHOTO_DIRECTORY, !Hash::make($user->id).'.jpg'
