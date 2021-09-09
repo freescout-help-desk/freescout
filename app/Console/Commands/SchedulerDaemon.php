677aabf0d4a52a716b97bcb152c63615e27b8666
lucharrier <lucharrier@gmail.com>
2021-09-09 13:33:30 +0200

    Comment while true

--- app/Console/Commands/SchedulerDaemon.php
+++ app/Console/Commands/SchedulerDaemon.php
@@ -31,7 +31,7 @@ class SchedulerDaemon extends Command
   {
       while (true) {
           $this->line('<info>[' . Carbon::now()->format('Y-m-d H:i:s') . ']</info> Calling scheduler');
-          $this->call('schedule:run');
+          // $this->call('schedule:run');
           sleep($this->option('sleep'));
       }
   }
