a51cb49225ff943f63b2a49800efaa0bc8670338
FreeScout <freescout.helpdesk@gmail.com>
2022-01-05 04:54:53 -0800

    Redirect to /home after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
