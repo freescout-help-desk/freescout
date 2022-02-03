345cdb2702d46f03c0e295b25127c11b43452b87
FreeScout <freescout.helpdesk@gmail.com>
2022-02-03 03:57:01 -0800

    v1.8.0

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '1.7.30',
+    'version' => '1.8.0',
 
     /*
     |--------------------------------------------------------------------------
