d26ab69b18c6a4593cdc433907ae7074834e6e9d
FreeScout <freescout.helpdesk@gmail.com>
2022-02-22 22:21:01 -0800

    Fix underscores in encoded subjects

--- overrides/webklex/laravel-imap/src/IMAP/Message.php
+++ overrides/webklex/laravel-imap/src/IMAP/Message.php
@@ -331,11 +331,11 @@ class Message
             if (\Str::startsWith(mb_strtolower($this->subject), '=?utf-8?')) {
 
                 // https://bugs.php.net/bug.php?id=68821
-                $header->subject = preg_replace_callback('/(=\?[^\?]+\?Q\?)([^\?]+)(\?=)/i', function($matches) {
+                $this->subject = preg_replace_callback('/(=\?[^\?]+\?Q\?)([^\?]+)(\?=)/i', function($matches) {
                     return $matches[1] . str_replace('_', '=20', $matches[2]) . $matches[3];
                 }, $header->subject);
 
-                $this->subject = mb_decode_mimeheader($header->subject);
+                $this->subject = mb_decode_mimeheader($this->subject);
             }
         }
 
