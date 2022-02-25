c0fc874ce431380f5bca712a2f420fc926882562
FreeScout <freescout.helpdesk@gmail.com>
2022-02-25 06:25:19 -0800

    If custom login page path set do not show login form at /login - closes #1751

--- overrides/laravel/framework/src/Illuminate/Routing/Router.php
+++ overrides/laravel/framework/src/Illuminate/Routing/Router.php
@@ -1120,8 +1120,9 @@ class Router implements RegistrarContract, BindingRegistrar
     public function auth()
     {
         // Authentication Routes...
-        $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-        $this->post('login', 'Auth\LoginController@login');
+        // login routes moved to web.php
+        //$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+        //$this->post('login', 'Auth\LoginController@login');
         $this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
         // Registration Routes...
