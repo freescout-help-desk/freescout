0e85c73a773fdb684c63131448db9e001e2f11c9
Arash Khajelou <a.khajelou@gmail.com>
2022-03-12 15:19:06 +0330

    Fix the key name for locales_rtl config.

--- app/Misc/Helper.php
+++ app/Misc/Helper.php
@@ -1616,6 +1616,6 @@ class Helper
 
     public static function isLocaleRtl(): bool
     {
-        return in_array(app()->getLocale(), config("app.rtl_locales"));
+        return in_array(app()->getLocale(), config("app.locales_rtl"));
     }
 }
