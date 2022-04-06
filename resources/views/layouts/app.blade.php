292e9ec0aea3cc4c1ef7d1a1a7ca867e09fa12e8
FreeScout <freescout.helpdesk@gmail.com>
2022-04-05 22:19:24 -0700

    Add noindex meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="robots" content="noindex,nofollow">
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
