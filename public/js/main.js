eff6c8c3516aaa4d6d3324bc42dae3e984d4d553
Moritz 'e1mo' Fromm <git@e1mo.de>
2022-04-14 12:31:46 +0200

    Add customer.company variable to templating
    A corresponding Feature-Request exists here:
    https://feedback.userreport.com/25a3cb5f-e4bd-4470-b6f3-79fcfaa8e90f

--- public/js/main.js
+++ public/js/main.js
@@ -181,6 +181,7 @@ var EditorInsertVarButton = function (context) {
 		        '<option value="{%customer.firstName%}">'+Lang.get("messages.first_name")+'</option>'+
 		        '<option value="{%customer.lastName%}">'+Lang.get("messages.last_name")+'</option>'+
 		        '<option value="{%customer.email%}">'+Lang.get("messages.email_addr")+'</option>'+
+		        '<option value="{%customer.company%}">'+Lang.get("messages.company_name")+'</option>'+
 		    '</optgroup>'+
 		    '<optgroup label="'+Lang.get("messages.user")+'">'+
 		        '<option value="{%user.fullName%}">'+Lang.get("messages.full_name")+'</option>'+
