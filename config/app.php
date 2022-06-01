338df4aa44884f07d56adc90519fcc14fb0cc95f
FreeScout <freescout.helpdesk@gmail.com>
2022-06-01 04:51:09 -0700

    Add Korean language

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 $key = env('APP_KEY', null);
 $key_file = env("APP_KEY_FILE", null);
 if (empty($key) && !empty($key_file)) {
-  $key = trim(file_get_contents($key_file));
+    $key = trim(file_get_contents($key_file));
 }
 
 return [
@@ -98,7 +98,7 @@ return [
     */
 
     'locale'          => env('APP_LOCALE', 'en'),
-    'locales'         => ['en', 'hr', 'cs', 'da', 'nl', 'fr', 'de', 'ja', 'it', 'fa', 'pl', 'pt-PT', 'pt-BR', 'ru', 'es', 'sk', 'sv'],
+    'locales'         => ['en', 'hr', 'cs', 'da', 'nl', 'fr', 'de', 'it', 'ja', 'ko', 'fa', 'pl', 'pt-PT', 'pt-BR', 'ru', 'es', 'sk', 'sv'],
     'locales_rtl'     => ['fa'],
     'default_locale'  => 'en',
 
