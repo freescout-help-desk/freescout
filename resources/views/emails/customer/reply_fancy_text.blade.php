d41000fb8d46b413c74b24fe8c4148df5f7d37a8
FreeScout <freescout.helpdesk@gmail.com>
2022-09-28 10:56:09 -0700

    Improve including email address of original sender when forwarding conversation - closes #2285

--- resources/views/emails/customer/reply_fancy_text.blade.php
+++ resources/views/emails/customer/reply_fancy_text.blade.php
@@ -1,9 +1,9 @@
 @if ($mailbox->before_reply){{ $mailbox->getReplySeparator() }}
 
-@endif
+@endif@php $is_forwarded = !empty($threads[0]) ? $threads[0]->isForwarded() : false; @endphp
 @foreach ($threads as $thread)
 @if (!$loop->first)-----------------------------------------------------------
-## {{--@if ($loop->last){{ __(':person sent a message', ['person' => $thread->getFromName($mailbox)]) }}@else {{ __(':person replied', ['person' => $thread->getFromName($mailbox)]) }}@endif--}}{{ $thread->getFromName($mailbox) }}, {{ __('on :date', ['date' => App\Customer::dateFormat($thread->created_at, 'M j @ H:i')]) }} ({{ \Config::get('app.timezone') }}):@endif 
+## {{--@if ($loop->last){{ __(':person sent a message', ['person' => $thread->getFromName($mailbox)]) }}@else {{ __(':person replied', ['person' => $thread->getFromName($mailbox)]) }}@endif--}}{{ $thread->getFromName($mailbox) }}@if ($is_forwarded && $thread->from) <{{ $thread->from }}>@endif, {{ __('on :date', ['date' => App\Customer::dateFormat($thread->created_at, 'M j @ H:i')]) }} ({{ \Config::get('app.timezone') }}):@endif 
 {{-- Html2Text\Html2Text::convert($thread->body) - this was causing "AttValue: " expected in Entity" error sometimes --}}{{ \Helper::htmlToText($thread->body, true) }}
 @if ($thread->source_via == App\Thread::PERSON_USER && \Eventy::filter('reply_email.include_signature', true, $thread))
 
