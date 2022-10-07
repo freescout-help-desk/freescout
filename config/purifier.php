41a0e0f205113cf7bc598e56b6c0c041d1034a38
FreeScout <freescout.helpdesk@gmail.com>
2022-10-07 06:41:24 -0700

    Preserve table classes when filtering email content - closes #2308

--- config/purifier.php
+++ config/purifier.php
@@ -24,7 +24,7 @@ return [
     'settings'      => [
         'default' => [
             'HTML.Doctype'             => 'HTML 4.01 Transitional',
-            'HTML.Allowed'             => 'div,b,strong,i,em,u,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src],table,tr,td,th,thead,tfoot,tbody,blockquote,pre,h1,h2,h3,h4,h5,h6',
+            'HTML.Allowed'             => 'div,b,strong,i,em,u,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src],table[class],tr,td,th,thead,tfoot,tbody,blockquote,pre,h1,h2,h3,h4,h5,h6',
             //'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
             'CSS.AllowedProperties'    => 'font-weight,font-style,text-decoration,color,background-color,text-align',
             'AutoFormat.AutoParagraph' => true,
