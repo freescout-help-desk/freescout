96b0c17bc82e511f6f81b7e4f1530d9a12f7f690
FreeScout <freescout.helpdesk@gmail.com>
2022-12-07 21:22:37 -0800

    Rename MYSQL_ATTR_SSL_CA attr into DB_MYSQL_ATTR_SSL_CA

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'strict'      => false,
             'engine'      => null,
             'options'     => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('DB_MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
