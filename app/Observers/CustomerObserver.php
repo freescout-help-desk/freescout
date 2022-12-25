f5aba6a0121b8cf04ad4a8087e080309b704d52d
FreeScout <freescout.helpdesk@gmail.com>
2022-12-25 09:16:24 -0800

    Update customer observer

--- app/Observers/CustomerObserver.php
+++ app/Observers/CustomerObserver.php
@@ -10,11 +10,6 @@ class CustomerObserver
     {
         $phones = $customer->getPhones();
         // Set numeric phones.
-        foreach ($phones as $i => $phone_data) {
-            if (!empty($phone_data['value'])) {
-                $phones[$i]['n'] = \Helper::phoneToNumeric($phone_data['value']);
-            }
-        }
-        $customer->phones = \Helper::jsonEncodeUtf8($phones);
+        $customer->setPhones($phones);
     }
 }
