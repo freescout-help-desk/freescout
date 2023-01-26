5ca4f39da4ee33c40e621bb60d93990f96c83e1f
FreeScout <freescout.helpdesk@gmail.com>
2023-01-25 21:52:59 -0800

    Add vendor files to dist

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'freescout-helpdesk/freescout',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'b504bf0ecd357c7125f83fa8dd02e2caadfc99c4',
+        'reference' => '475561cbf53f21d2ab1ac028aa6f0dd7af3c7a9a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -166,7 +166,7 @@
         'freescout-helpdesk/freescout' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'b504bf0ecd357c7125f83fa8dd02e2caadfc99c4',
+            'reference' => '475561cbf53f21d2ab1ac028aa6f0dd7af3c7a9a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
