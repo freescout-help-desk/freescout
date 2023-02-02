27deddef1e629580d56981cdeb61b5fe8948bb8b
FreeScout <freescout.helpdesk@gmail.com>
2023-02-02 05:18:48 -0800

    Make cards wider - #2663

--- resources/views/customers/partials/customers_table.blade.php
+++ resources/views/customers/partials/customers_table.blade.php
@@ -5,7 +5,7 @@
                 @foreach ($customers as $customer)
                     <a href="{{ Eventy::filter('customer.card.url', route('customers.update', ['id' => $customer->id]), $customer) }}" class="card hover-shade" @action('customer.card.link', $customer) >
                         <img src="{{ $customer->getPhotoUrl() }}" />
-                        <h4>{{ $customer->first_name }} {{ $customer->last_name }}</h4>
+                        <h4 title="{{ $customer->first_name }} {{ $customer->last_name }}">{{ $customer->first_name }} {{ $customer->last_name }}</h4>
                         <p class="text-truncate"><small>{{ $customer->getEmailOrPhone() }}</small></p>
                     </a>
                 @endforeach
