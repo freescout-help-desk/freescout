969e27a14b4cfc80ba12fbee769974185db110d7
Avenjamin <avenjamin@users.noreply.github.com>
2023-02-14 12:17:29 +1100

    Add theme-color meta tag @filter
    Allow overriding of the theme-color <meta> tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link rel="manifest" href="/site.webmanifest" crossorigin="use-credentials">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
+    <meta name="theme-color" content="@filter('layout.theme_color', '#ffffff'))">
     @action('layout.head')
     {{-- Styles --}}
     {{-- Conversation page must open immediately, so we are loading scripts present on conversation page --}}
