cd9a024b026a2dff46f64b874de79b1edf7c27eb
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-05-10 00:54:50 -0400

    refs#32 fixed conversation naming issue

--- Modules/Reports/Http/routes.php
+++ Modules/Reports/Http/routes.php
@@ -2,7 +2,7 @@
 
 Route::group(['middleware' => 'web', 'prefix' => \Helper::getSubdirectory(), 'namespace' => 'Modules\Reports\Http\Controllers'], function()
 {
-    Route::get('/reports/conversations', ['uses' => 'ReportsController@conversationsReport', 'middleware' => ['auth', 'roles'], 'roles' => ['user', 'admin']])->name('reports.conversations');
+    Route::get('/reports/tickets', ['uses' => 'ReportsController@conversationsReport', 'middleware' => ['auth', 'roles'], 'roles' => ['user', 'admin']])->name('reports.conversations');
     Route::get('/reports/productivity', ['uses' => 'ReportsController@productivityReport', 'middleware' => ['auth', 'roles'], 'roles' => ['user', 'admin']])->name('reports.productivity');
     Route::get('/reports/satisfaction', ['uses' => 'ReportsController@satisfactionReport', 'middleware' => ['auth', 'roles'], 'roles' => ['user', 'admin']])->name('reports.satisfaction');
     Route::get('/reports/time-tracking', ['uses' => 'ReportsController@timeReport', 'middleware' => ['auth', 'roles'], 'roles' => ['user', 'admin']])->name('reports.time');
