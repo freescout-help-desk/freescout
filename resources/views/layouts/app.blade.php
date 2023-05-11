5af5f6575f9b2c7c6eee58d6aec3b23eee06b595
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-05-11 12:07:21 -0400

    removed unused code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -312,8 +312,6 @@
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
     <script>
     $(document).ready(function() {
         $('.datatable').DataTable({
