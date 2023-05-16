a1a35c2749dca511ffafc38a14ee4a163581c828
Azim Pathan <azimasifpathan@gmail.com>
2023-05-16 17:40:59 +0530

    Canidesk-30 Dashboard > Create report dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -185,4 +185,171 @@ populateUl();
 
 </style>
 
+
+
+<!---charts start-->
+<html>
+<body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" charset="utf-8"></script>
+<div class="chart">
+    <ul class="numbers">
+        <li><span>8%</span></li>
+        <li><span>6%</span></li>
+        <li><span>4%</span></li>
+        <li><span>2%</span></li>
+        <li><span>0%</span</li>
+</ul>
+<ul class="bars">
+    <h2 class="header">Weekly Tickets</h2>
+<li><div class="bar" data-percentage="30"></div><span>S</span></li>
+<li><div class="bar" data-percentage="50"></div><span>M</span></li>
+<li><div class="bar" data-percentage="40"></div><span>T</span></li>
+<li><div class="bar" data-percentage="50"></div><span>W</span></li>
+<li><div class="bar" data-percentage="80"></div><span>T</span></li>
+<li><div class="bar" data-percentage="20"></div><span>F</span></li>
+<li><div class="bar" data-percentage="30"></div><span>S</span></li>
+<h2 class="head">Average resolved tickets</h2>
+</ul>
+</div>
+
+<script type="text/javascript">
+$(function(){
+    $('.bars li .bar').each(function(key, bar){
+        var percentage = $(this).data('percentage');
+        $(this).animate({
+            'height' : percentage + "%"
+        },1000);
+        });
+    });
+
+
+</script>
+</body>
+</html>
+<style>
+bars{
+    margin : 0;
+    padding: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-item: center;
+    font-family: "Roboto", sans-serif;
+    background: #333;
+}
+.header{
+    color: #fff;
+    width:34%;
+    position: absolute;
+    bottom:383px;
+    text-align:center;  
+}
+.head{
+    color: #fff;
+    width:125%;
+    position: absolute;
+    bottom:4px;
+    text-align:center;
+   
+}
+
+.charts{
+    width: 600px;
+    height: 300px;
+    display: block;
+
+}
+
+.numbers{
+    color: #fff;
+    margin: 0;
+    padding: 0;
+    width: 50px;
+    height: 31px;
+    display: inline-block;
+    float: left;
+}
+.numbers li{
+    list-style: none;
+    height: 110px;
+    position: relative;
+    bottom: 145px;
+}
+
+.numbers span{
+    font-size: 12px;
+    font-weight: 600;
+    position: absolute;
+    bottom: 0;
+    right: 10px;
+}
+
+.bars{
+    color: #fff;
+    font-size: 12px;
+    font-weight: 600;
+    background: #555;
+    margin: 0;
+    padding: 0;
+    display: inline-block;
+    width: 1797px;
+    height: 431px;
+    box-shadow: 0 0 10px 0 #555;
+    border-radius: 19px;
+}
+ .bars li{
+    display: table-cell;
+    width: 272px;
+    height: 361px;
+    position: relative;
+ }
+
+ .bars span{
+    width: 117%;
+    position: absolute;
+    bottom: -25px;
+    text-align: center;
+ }
+
+ .bars .bar{
+    display: block;
+    background: #17C0EB;
+    width: 80px;
+    
+    position: absolute;
+    bottom: 0;
+    margin-left: 113px;
+    text-align: center;
+    box-shadow: 0 0 10px 0 rgba(23, 192, 235, 0.5);
+    transition: 0.5s;
+    transition-property: background, box-shadow;
+ }
+
+ .bars .bar:hover{
+    background: #55EFC4;
+    box-shadow: 0 0 10px 0 rgba(85, 239, 196, 0.5);
+    cursor: pointer;
+ }
+
+ .bars .bar:before{
+    color: #fff;
+    content: attr(data-percentage);
+    position: relative;
+    bottom: 20px;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+    </style>
+
+
 @endsection
