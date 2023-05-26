e968cac7e25d9ecc6cd2a9dd7742df6f2333ae6c
pravinpimpale <pravinpimpale45@gmail.com>
2023-05-26 19:17:23 +0530

    add Refresh button and fixed dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Conversation;
+use App\Mailbox;
 use Carbon\Carbon;
 use Exception;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Modules\Reports\Http\Controllers\ReportsController;
@@ -14,30 +16,67 @@ use Modules\Reports\Http\Controllers\ReportsController;
 class DashboardController extends Controller
 {
     //Index Route
-    public function index()
+    public function index(Request $request)
     {
         $today = Carbon::today();
         $fourDaysAgo = Carbon::today()->subDays(4);
         $sevenDaysAgo = Carbon::today()->subDays(7);
         $thirtyDaysAgo = Carbon::today()->subDays(30);
 
-        $results = Conversation::select(
+        $ticketFilter=$request->input('ticket');
+        $productFilter=$request->input('product');
+        $typeFilter=$request->input('type');
+        $mailboxId=$request->input('mailbox');
+
+        $dateFromFilter=$request->input('from');
+        $dateToFilter=$request->input('to');
+
+        // Make empty query
+        $query = Conversation::query();
+
+        // Filtering based on Mailbox selected
+        if(!empty($mailboxId)){
+            $query = $query->where('mailbox_id', $mailboxId);
+        }
+        if(!empty($typeFilter)){
+            $query = $query->where('type', $typeFilter);
+        }
+        //TODO : need to fixed this filter
+        // if(!empty($ticketFilter)){
+        //     $query = $query->where('mailbox_id', $ticketFilter);
+        // }
+        // if(!empty($productFilter)){
+        //     $query = $query->where('mailbox_id', $productFilter);
+        // }
+        // if(!empty($dateFromFilter)){
+        //     $query = $query->where('created_at', $dateFromFilter);
+        // }
+        // if(!empty($dateToFilter)){
+        //     $query = $query->where('updated_at', $dateToFilter);
+        // }
+
+        // Extract the data
+        $results = $query->select(
             DB::raw('COUNT(*) as total_count'),
             DB::raw('COUNT(CASE WHEN created_by_user_id IS NULL THEN 1 END) as unassigned_count'),
-            DB::raw('COUNT(CASE WHEN created_at < ? AND closed_at IS NULL THEN 1 END) as overdue_count'),
+            // DB::raw('COUNT(CASE WHEN closed_at IS NULL THEN 1 END) as overdue_count'),
+            // DB::raw('COUNT(CASE WHEN created_at < ? AND closed_at IS NULL THEN 1 END) as overdue_count'),
             DB::raw('COUNT(CASE WHEN closed_at IS NULL THEN 1 END) as unclosed_count'),
-            DB::raw('COUNT(CASE WHEN closed_at IS NOT NULL AND created_at < ? THEN 1 END) as closed_tickets_count'),
-            DB::raw('COUNT(CASE WHEN closed_at IS NULL AND created_at < ? THEN 1 END) as unclosed_created_30_days_ago_count')
+            DB::raw('COUNT(CASE WHEN closed_at IS NOT NULL THEN 1 END) as closed_tickets_count'),
+            DB::raw('COUNT(CASE WHEN closed_at IS NULL THEN 1 END) as unclosed_created_30_days_ago_count')
         )
-            ->setBindings([$today, $fourDaysAgo, $sevenDaysAgo, $thirtyDaysAgo])
-            ->first();
+        ->first();
+
 
         $totalCount = $results->total_count;
         $unassignedCount = $results->unassigned_count;
-        $overdueCount = $results->overdue_count;
+        $overdueCount = 0;
         $unclosedCount = $results->unclosed_count;
         $closedCount = $results->closed_tickets_count;
         $unclosedCreated30DaysAgoCount = $results->unclosed_created_30_days_ago_count;
+
+        $filters['to'] = User::dateFormat(date('Y-m-d H:i:s'), 'Y-m-d', null, false);
+        $filters['from'] = date('Y-m-d', strtotime($filters['to'].' -1 week'));
 
         // For Weekly data
         $startDate = now()->startOfWeek();
@@ -61,7 +100,6 @@ class DashboardController extends Controller
             ->where('name', 'Ticket Category')
             ->pluck('options');
 
-        // var_dump($values);die();
         $categoryValues = [];
         if(!empty($values)){
             try{
@@ -72,8 +110,25 @@ class DashboardController extends Controller
             }catch(Exception $ex){
 
             }
+            // Product value Labels
+        $values = DB::table('custom_fields')
+            ->where('name', 'Product')
+            ->pluck('options');
+
+        $productValues = [];
+        if(!empty($values)){
+            try{
+                $options = json_decode($values[0], true);
+                foreach ($options as $key => $value) {
+                    array_push($productValues, $value);
+                }
+            }catch(Exception $ex){
+
+            }
+        }
+
         }
         // Category Tickets
-        return view('/dashboard/dashboard', compact('totalCount', 'unassignedCount', 'overdueCount', 'unclosedCount', 'closedCount', 'unclosedCreated30DaysAgoCount', 'tickets', 'categoryValues'));
+        return view('/dashboard/dashboard', compact('totalCount', 'unassignedCount', 'overdueCount', 'unclosedCount', 'closedCount', 'unclosedCreated30DaysAgoCount', 'tickets', 'categoryValues','filters','productValues'));
     }
 }
