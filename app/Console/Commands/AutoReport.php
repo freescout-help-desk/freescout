61171064c964f4064ee151c6d41f7332f9b6e7f2
rajeshrathod972 <rr7049908@gmail.com>
2023-06-20 01:38:32 +0530

    sla report pdf ganerating partially done

--- app/Console/Commands/AutoReport.php
+++ app/Console/Commands/AutoReport.php
@@ -3,7 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Settingssla;
+use App\SLASetting;
+use App\Conversation;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use \PDF;
 class AutoReport extends Command
 {
     /**
@@ -18,7 +22,7 @@ class AutoReport extends Command
      *
      * @var string
      */
-    protected $description = 'this command is uesd to send email automatically';
+    protected $description = 'this command is used to send email automatically';
 
     /**
      * Create a new command instance.
@@ -40,7 +44,35 @@ class AutoReport extends Command
     public function handle()
     {
 
-        $settings=Settingssla::orderBy('id', 'desc')->first();
-        $this->info($settings);
+        // $settings=SLASetting::orderBy('id', 'desc')->first();
+        // $email=explode(',', $settings->to_email);
+        // $this->info($settings);
+        $tickets = Conversation::with('user', 'conversationCustomField.custom_field', 'conversationCategory','conversationPriority')->get();
+        $dompdf = new Dompdf();
+        // (Optional) Set Dompdf options
+        $options = new Options();
+        $options->set('defaultFont', 'Arial'); // Set the default font
+        // You can customize other options if needed
+
+        $dompdf->setOptions($options);
+
+        // Load the Blade view with the table data
+        $html = view('sla.report-email', compact('tickets'));
+        // Load the HTML content
+        $dompdf->loadHtml($html);
+
+        // Render the PDF
+        $dompdf->render();
+
+        // Output the generated PDF to the browser
+    $dompdf->save('report.pdf');
+
+        $data = array('name'=>"Example");
+        // $mail = Mail::send('mail', $data, function($message) {
+        // $message->to($settings->to_email, 'Tutorials Point')->subject
+        //     ('hello i am rajesh rathod')->attach('/home/rathod/Downloads/report.pdf');
+        // $message->from('hello@example.com','Example');
+        
+        // });
     }
 }
