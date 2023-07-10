3d7f424b28e1e1df1dfc21287fa962db8e8796ab
FreeScout <freescout.helpdesk@gmail.com>
2023-07-09 22:48:33 -0700

    Limit the number of displayed non writable cache files on Status page - closes #3165

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -55,6 +55,8 @@ class SystemController extends Controller
         if (function_exists('shell_exec')) {
             $non_writable_cache_file = shell_exec('find '.base_path('storage/framework/cache/data/').' -type f | xargs -I {} sh -c \'[ ! -w "{}" ] && echo {}\' 2>&1 | head -n 1');
             $non_writable_cache_file = trim($non_writable_cache_file ?? '');
+            // Leave only one line (in case head -n 1 does not work)
+            $non_writable_cache_file = preg_replace("#[\r\n].+#m", '', $non_writable_cache_file);
             if (!strstr($non_writable_cache_file, base_path('storage/framework/cache/data/'))) {
                 $non_writable_cache_file = '';
             }
