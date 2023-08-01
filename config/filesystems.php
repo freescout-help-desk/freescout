20b8c077dd22d8e6838fdf05ce99c94abf303036
Erki Aas <erki@veebkolm.ee>
2023-08-01 22:23:39 +0300

    fix(s3): add configuration for use_path_style_endpoint

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,6 +71,7 @@ return [
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'endpoint' => env('AWS_ENDPOINTS'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
     ],
