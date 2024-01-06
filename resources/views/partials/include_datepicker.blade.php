08e50e3643796dd18b1257c978d1cec6ac1bd571
FreeScout <freescout.helpdesk@gmail.com>
2024-01-06 03:55:51 -0800

    Localize calendar

--- resources/views/partials/include_datepicker.blade.php
+++ resources/views/partials/include_datepicker.blade.php
@@ -11,8 +11,12 @@
 	    {!! Minify::javascript(['/js/flatpickr/flatpickr.min.js', '/js/flatpickr/l10n/'.strtolower(Config::get('app.locale')).'.js']) !!}
 	    {{-- Default config should be defined here because if "javascript" section is used when new datepicker is loaded it redefined the default config. --}}
 	    @if (\Helper::isTimeFormat24())
+	    	@php $flatpickr_locale = preg_replace("#\-.*#", '', Config::get('app.locale')); @endphp
 		    <script type="text/javascript" {!! \Helper::cspNonceAttr() !!}>
 		    	flatpickr.defaultConfig.time_24hr = true;
+		    	if (typeof(flatpickr.l10ns['{{ $flatpickr_locale }}']) != "undefined") {
+		    		flatpickr.localize(flatpickr.l10ns['{{ $flatpickr_locale }}']);
+		    	}
 		    </script>
 		@endif
 	@endif
