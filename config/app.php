0299c8be99f486872346093466616cb01de1d214
FreeScout <freescout.helpdesk@gmail.com>
2024-05-10 22:46:33 -0700

    Switch to the new fetching library

--- config/app.php
+++ config/app.php
@@ -398,7 +398,7 @@ return [
     | Library used to fetch emails: webklex/laravel-imap, webklex/php-imap
     |-------------------------------------------------------------------------
     */
-    'new_fetching_library'    => env('APP_NEW_FETCHING_LIBRARY', false),
+    'new_fetching_library'    => env('APP_NEW_FETCHING_LIBRARY', true),
 
     /*
     |--------------------------------------------------------------------------
