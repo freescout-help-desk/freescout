6830a78e2313fddeb046321c6cf080dae7a42dab
FreeScout <freescout.helpdesk@gmail.com>
2024-05-22 22:04:29 -0700

    Show exclamation icon in mailbox connection settings tabs

--- resources/views/mailboxes/connection_menu.blade.php
+++ resources/views/mailboxes/connection_menu.blade.php
@@ -2,6 +2,6 @@
 	@php
 		$route_name = Route::currentRouteName();
 	@endphp
-    <li @if ($route_name == 'mailboxes.connection')class="active"@endif><a href="{{ route('mailboxes.connection', ['id'=>$mailbox->id]) }}">@if ($route_name != 'mailboxes.connection' && !$mailbox->isInActive())<i class="glyphicon glyphicon-exclamation-sign text-warning"></i> @endif{{ __('Sending Emails') }}</a></li>
-    <li @if ($route_name == 'mailboxes.connection.incoming')class="active"@endif><a href="{{ route('mailboxes.connection.incoming', ['id'=>$mailbox->id]) }}">@if ($route_name != 'mailboxes.connection.incoming' && !$mailbox->isInActive())<i class="glyphicon glyphicon-exclamation-sign text-warning"></i> @endif {{ __('Fetching Emails') }}</a></li>
+    <li @if ($route_name == 'mailboxes.connection')class="active"@endif><a href="{{ route('mailboxes.connection', ['id'=>$mailbox->id]) }}">@if ($route_name != 'mailboxes.connection' && !$mailbox->isOutActive())<i class="glyphicon glyphicon-exclamation-sign"></i> @endif{{ __('Sending Emails') }}</a></li>
+    <li @if ($route_name == 'mailboxes.connection.incoming')class="active"@endif><a href="{{ route('mailboxes.connection.incoming', ['id'=>$mailbox->id]) }}">@if ($route_name != 'mailboxes.connection.incoming' && !$mailbox->isInActive())<i class="glyphicon glyphicon-exclamation-sign"></i> @endif {{ __('Fetching Emails') }}</a></li>
 </ul>
