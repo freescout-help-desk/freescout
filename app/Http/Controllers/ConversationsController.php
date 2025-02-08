bb178de5d2d48169928e655f7cea75f4988b4d91
FreeScout <freescout.helpdesk@gmail.com>
2025-02-08 07:48:15 -0800

    Fix searching conversations and customers on PostgreSQL - closes #4559

--- app/Http/Controllers/ConversationsController.php
+++ app/Http/Controllers/ConversationsController.php
@@ -3021,7 +3021,7 @@ class ConversationsController extends Controller
 
                 $query->where('customers.first_name', $like_op, $like)
                     ->orWhere('customers.last_name', $like_op, $like)
-                    ->orWhere(\DB::raw('CONCAT(customers.first_name, " ", customers.last_name)'), $like_op, $like)
+                    ->orWhere(\Helper::isPgSql() ? \DB::raw('(customers.first_name || \' \' || customers.last_name)') : \DB::raw('CONCAT(customers.first_name, " ", customers.last_name)'), $like_op, $like)
                     ->orWhere('customers.company', $like_op, $like)
                     ->orWhere('customers.job_title', $like_op, $like)
                     ->orWhere('customers.websites', $like_op, $like)
