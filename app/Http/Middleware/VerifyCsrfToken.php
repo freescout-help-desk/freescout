f9f491c45305ac0a1311d9ae5ec4c2d17882a00f
FreeScout <freescout.helpdesk@gmail.com>
2025-05-14 06:38:18 -0700

    Fix TokenMismatchException not found - closes #4753

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
