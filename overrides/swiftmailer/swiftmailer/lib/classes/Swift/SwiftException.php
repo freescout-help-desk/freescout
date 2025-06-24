b3e64dcfb163079e91854de2b08bc6a5856649b5
FreeScout <freescout.helpdesk@gmail.com>
2025-06-23 22:58:10 -0700

    PHP 8.4 compatibility - closes #4881

--- overrides/swiftmailer/swiftmailer/lib/classes/Swift/SwiftException.php
+++ overrides/swiftmailer/swiftmailer/lib/classes/Swift/SwiftException.php
@@ -21,7 +21,7 @@ class Swift_SwiftException extends Exception
      * @param string $message
      * @param int    $code
      */
-    public function __construct($message, $code = 0, Exception $previous = null)
+    public function __construct($message, $code = 0, ?Exception $previous = null)
     {
         parent::__construct($message, $code ?? 0, $previous);
     }
