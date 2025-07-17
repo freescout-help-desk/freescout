62768d2112c1359205bfbc2bbd3e6b46dae0f4bb
FreeScout <freescout.helpdesk@gmail.com>
2025-07-17 12:14:46 -0700

    Added actions to system status and tools pages - closes #4920

--- resources/views/system/tools.blade.php
+++ resources/views/system/tools.blade.php
@@ -15,8 +15,12 @@
 
 <div class="container">
 
+    @action('system.tools.before_form')
+
     <form class="form-inline margin-top" method="POST" action="">
         {{ csrf_field() }}
+
+        @action('system.tools.form_start')
 
         <div>
             <button type="submit" class="btn btn-default" name="action" value="clear_cache">
@@ -30,7 +34,11 @@
             <button type="submit" class="btn btn-default" name="action" value="logout_users">
                 {{ __('Logout Users') }}
             </button>
+            @action('system.tools.main_buttons')
         </div>
+
+        @action('system.tools.after_main_buttons')
+
         <hr/>
         <div class="margin-top text-help">
             <button type="submit" class="btn btn-default" name="action" value="fetch_emails">
@@ -41,20 +49,29 @@
             {{ __('Days') }}: <input type="number" name="days" value="{{ old('days', 3) }}" class="form-control input-sm" />
 
             &nbsp;
-            <input type="radio" value="1" name="unseen" @if ((int)old('unseen', 1)) checked @endif /> {{ __('Unread') }}   
+            <input type="radio" value="1" name="unseen" @if ((int)old('unseen', 1)) checked @endif /> {{ __('Unread') }}
             &nbsp;
             <input type="radio" value="0" name="unseen" @if (!(int)old('unseen', 1)) checked @endif /> {{ __('All') }}
 
             &nbsp;
-            <input type="checkbox" value="1" name="debug" @if ((int)old('debug')) checked @endif /> {{ __('Debug') }}   
-            
+            <input type="checkbox" value="1" name="debug" @if ((int)old('debug')) checked @endif /> {{ __('Debug') }}
+
+            @action('system.tools.fetch_emails_append')
         </div>
-            
+
+        @action('system.tools.form_append')
+
     </form>
 
+    @action('system.tools.after_form')
+
     @if ($output)
+        @action('system.tools.before_output')
         <div class="console margin-top">{{ $output }}</div>
+        @action('system.tools.after_output')
     @endif
+
+    @action('system.tools.after_content')
 
 </div>
 @endsection
