19d306454ee03bec105c4ac4762e9a196c9b26a1
FreeScout <freescout.helpdesk@gmail.com>
2025-12-29 08:44:22 -0800

    Add array_first_cond() function

--- overrides/laravel/framework/src/Illuminate/Support/helpers.php
+++ overrides/laravel/framework/src/Illuminate/Support/helpers.php
@@ -115,6 +115,19 @@ if (! function_exists('array_first')) {
     }
 }
 
+/**
+ * Return the first element in an array passing a given truth test.
+ *
+ * @param  array  $array
+ * @param  callable|null  $callback
+ * @param  mixed  $default
+ * @return mixed
+ */
+function array_first_cond($array, ?callable $callback = null, $default = null)
+{
+    return Arr::first($array, $callback, $default);
+}
+
 if (! function_exists('array_flatten')) {
     /**
      * Flatten a multi-dimensional array into a single level.
