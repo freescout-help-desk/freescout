2101f72f4e77c90cdb5e3c4188b83acc78f5f980
FreeScout <freescout.helpdesk@gmail.com>
2026-02-22 00:51:51 -0800

    PHP 8.3 compatibility

---
+++ overrides/laravel/framework/src/Illuminate/Session/NullSessionHandler.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Illuminate\Session;
+
+use SessionHandlerInterface;
+
+class NullSessionHandler implements SessionHandlerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function open($savePath, $sessionName): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function close(): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read($sessionId): string|false
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($sessionId, $data): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function destroy($sessionId): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function gc($lifetime): int|false
+    {
+        return true;
+    }
+}
