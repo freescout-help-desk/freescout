486a2b9c157aecee11fd98069caf2364bceb9e96
FreeScout <freescout.helpdesk@gmail.com>
2026-03-16 08:53:21 -0700

    Fix saving empty encrypted ENV values to via settings

--- app/Misc/Helper.php
+++ app/Misc/Helper.php
@@ -2436,7 +2436,7 @@ class Helper
         }
 
         //  frame-src https://recaptcha.net; connect-src https://recaptcha.net;
-
+        //  The frame-ancestors is ignored when delivered via a meta element.
         $csp = "<meta http-equiv=\"Content-Security-Policy\" content=\"base-uri 'none'; default-src 'self' ".self::sanitizeCsp($script_domains)."; img-src * 'self' data:; font-src * 'self' data:; style-src * 'self' 'unsafe-inline'; form-action 'self' ".self::sanitizeCsp(\Eventy::filter('csp.form_action', ''), true)."; frame-src * 'self'; script-src 'self' 'nonce-".$nonce."' "
             .self::sanitizeCsp($script_src).";"
             .self::sanitizeCsp(config('app.csp_custom').self::sanitizeCsp(\Eventy::filter('csp.custom', '')))."\">";
