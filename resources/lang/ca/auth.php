fe758675a1d4ee11b8296e95b70e41aa5c842e4d
Albert Simó <losimo@gmail.com>
2026-04-25 17:49:33 +0200

    Add Catalan (ca) translation
    Adds a complete Catalan translation for the FreeScout interface:
    - resources/lang/ca.json (810 keys, full coverage)
    - resources/lang/ca/auth.php
    - resources/lang/ca/passwords.php
    - resources/lang/ca/validation.php
    - config/app.php: added 'ca' to the locales array
    
    Translation follows Softcatalà guidelines (normative Catalan,
    vosaltres form for instructions, standard IT terminology).

---
+++ resources/lang/ca/auth.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'failed'   => 'Les credencials no coincideixen amb els nostres registres.',
+    'throttle' => 'Massa intents d\'inici de sessió. Torneu-ho a intentar en :seconds segons.',
+];
