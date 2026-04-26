285ca977fa30cab8c8bffb221ffa44cfc5ca0406
FreeScout <freescout.helpdesk@gmail.com>
2026-04-25 21:52:31 -0700

    Add Catalan tranlation - closes #5376

--- config/app.php
+++ config/app.php
@@ -98,7 +98,7 @@ return [
     */
 
     'locale'          => env('APP_LOCALE', 'en'),
-    'locales'         => ['en', 'ar', 'zh-CN', 'zh-TW', 'hr', 'cs', 'da', 'nl', 'fi', 'fr', 'de', 'he', 'hu', 'it', 'ja', 'kz', 'ko', 'no', 'fa', 'pl', 'pt-PT', 'pt-BR', 'ro', 'ru', 'es', 'sk', 'sl', 'sv', 'tr', 'uk'],
+    'locales'         => ['en', 'ar', 'ca', 'zh-CN', 'zh-TW', 'hr', 'cs', 'da', 'nl', 'fi', 'fr', 'de', 'he', 'hu', 'it', 'ja', 'kz', 'ko', 'no', 'fa', 'pl', 'pt-PT', 'pt-BR', 'ro', 'ru', 'es', 'sk', 'sl', 'sv', 'tr', 'uk'],
     'locales_rtl'     => ['ar', 'fa', 'he'],
     'default_locale'  => 'en',
 
---
+++ resources/lang/ca/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'password' => 'Les contrasenyes han de tenir almenys sis caràcters i coincidir amb la confirmació.',
+    'reset'    => 'S\'ha restablert la contrasenya!',
+    'sent'     => 'Hem enviat per correu l\'enllaç per restablir la contrasenya!',
+    'token'    => 'Aquest token per restablir la contrasenya no és vàlid.',
+    'user'     => 'No trobem cap usuari amb aquesta adreça de correu.',
+];
