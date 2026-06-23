36eb145dd84990965a1d9f7761eb6f235cee002b
Albert Simó <albert@losimo.cat>
2026-06-23 16:23:05 +0200

    fix: add htmlRequired Parsley validator for Summernote body field (#4590)

--- public/js/main.js
+++ public/js/main.js
@@ -90,6 +90,15 @@ $.extend(window.ParsleyConfig, {
     errorTemplate: '<div></div>'
 });
 
+// Custom validator for HTML editor fields (Summernote leaves <p><br></p> when emptied).
+// Used via data-parsley-html-required on the body textarea.
+window.Parsley.addValidator('htmlRequired', {
+    validateString: function(value) {
+        return !!$.trim(value.replace(/<[^>]+>/g, '').replace(/&nbsp;/gi, ''));
+    },
+    messages: { en: 'This field is required.' }
+});
+
 // Push notifications
 /*Push.config({
     serviceWorker: './customServiceWorker.js', // Sets a custom service worker script
@@ -1659,12 +1668,8 @@ function showReplyForm(data, scroll_offset)
 		$('#body').summernote('focus');
 	}
 
-	if (!isChatMode()) {
-		// Select2 for CC/BCC
-		initRecipientSelector();
-	} else {
-		$('form.form-reply:first .field-cc').addClass('hidden');
-	}
+	// Select2 for CC/BCC
+	initRecipientSelector();
 
 	if (typeof(scroll_offset) == "undefined") {
 		scroll_offset = 0;
@@ -3117,7 +3122,6 @@ function initMergeConv()
 			fsAjax({
 					action: 'merge_search',
 					number: $('.merge-conv-number:visible:first').val(),
-					cur_conv_id: getGlobalAttr('conversation_id')
 				},
 				laroute.route('conversations.ajax'),
 				function(response) {
