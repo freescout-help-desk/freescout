46bd96f44234ab4971fd1c197a1482ab6846600c
FreeScout <freescout.helpdesk@gmail.com>
2026-08-28 04:14:49 -0700

    Mark notifications as read by convesrsation_id field in notifications table - closes #5600

---
+++ database/migrations/2026_08_28_010101_add_conversation_id_column_to_notifications_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddConversationIdColumnToNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            // https://github.com/freescout-help-desk/freescout/issues/5600
+            $table->unsignedInteger('conversation_id')->after('data')->nullable();
+            // "read_at" is skipped on purpose - there is no use from it here.
+            $table->index(['notifiable_id', 'notifiable_type', 'conversation_id'], 'notifications_conversation_id');
+        });
+
+        // Populate "conversation_id" field for notifications.
+        $total = DatabaseNotification::whereNull('conversation_id')
+            ->where('data', 'like', '%"conversation_id":%')
+            ->count();
+        $bunch_size = 500;
+        for ($bunch_i = 0; $bunch_i < ceil($total / $bunch_size); $bunch_i++) { 
+            $notifications = DatabaseNotification::whereNull('conversation_id')
+                ->where('data', 'like', '%"conversation_id":%')
+                ->orderBy('id')
+                //->skip($bunch_i*$bunch_size)
+                ->limit($bunch_size)
+                ->get();
+            foreach ($notifications as $notification) {
+                if (!empty($notification->data['conversation_id']) && (int)$notification->data['conversation_id']) {
+                    $notification->conversation_id = (int)$notification->data['conversation_id'];
+                    $notification->save();
+                }
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->dropColumn('conversation_id');
+            $table->dropIndex('notifications_conversation_id');
+        });
+    }
+}
