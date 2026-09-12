3d5cffc50ce9af757590a6ea8d90dbdf7b693cdd
FreeScout <freescout.helpdesk@gmail.com>
2026-09-11 21:45:16 -0700

    Allow data: in frame-src CSP rule - closes #5630

--- app/Misc/Helper.php
+++ app/Misc/Helper.php
@@ -2957,7 +2957,8 @@ class Helper
 
         //  frame-src https://recaptcha.net; connect-src https://recaptcha.net;
         //  The frame-ancestors is ignored when delivered via a meta element.
-        $csp = "base-uri 'none'; default-src 'self' ".self::sanitizeCsp($script_domains)."; img-src * 'self' data:; font-src * 'self' data:; style-src * 'self' 'unsafe-inline'; form-action 'self' ".self::sanitizeCsp(\Eventy::filter('csp.form_action', ''), true)."; frame-src * 'self'; script-src 'self' 'nonce-".$nonce."' "
+        //  "data:" in framce-src: https://github.com/freescout-help-desk/freescout/issues/5630
+        $csp = "base-uri 'none'; default-src 'self' ".self::sanitizeCsp($script_domains)."; img-src * 'self' data:; font-src * 'self' data:; style-src * 'self' 'unsafe-inline'; form-action 'self' ".self::sanitizeCsp(\Eventy::filter('csp.form_action', ''), true)."; frame-src * 'self' data:; script-src 'self' 'nonce-".$nonce."' "
             .self::sanitizeCsp($script_src).";"
             .self::sanitizeCsp(config('app.csp_custom').self::sanitizeCsp(\Eventy::filter('csp.custom', '')));
 
